924a0e41899031d3696c9dc78014f3e71da6e858
brijeshtrigun <brijeshtrigun02@gmail.com>
2026-08-27 00:55:02 +0530

    Time: 0 ms (100.00%) | Memory: 10 MB (86.86%) - LeetSync

---
+++ 13-roman-to-integer/roman-to-integer.cpp
@@ -0,0 +1,30 @@
+class Solution {
+
+public:
+    int getValue(char c){
+    if(c == 'I') return 1;
+    if(c == 'V') return 5;
+    if(c == 'X') return 10;
+    if(c == 'L') return 50;
+    if(c == 'C') return 100;
+    if(c == 'D') return 500;
+    if(c == 'M') return 1000;
+    return 0;
+    }
+    int romanToInt(string s) {
+        int ans =0;
+        int i=0;
+        while(i< s.size()-1){
+            if(getValue(s[i]) < getValue(s[i+1])){
+                ans-= getValue(s[i]);
+
+            }
+else{
+            ans+=getValue(s[i]);
+           }
+              i++;
+    }
+        ans+=getValue(s[i]);
+        return ans;
+    }
+};
