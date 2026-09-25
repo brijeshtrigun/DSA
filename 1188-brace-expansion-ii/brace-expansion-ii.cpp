693def48aaacc7dc71f730ffbb2dd2d09fa8fef3
brijeshtrigun <brijeshtrigun02@gmail.com>
2026-09-25 23:07:36 +0530

    Time: 13 ms (33.04%) | Memory: 17.3 MB (33.04%) - LeetSync

---
+++ 1188-brace-expansion-ii/brace-expansion-ii.cpp
@@ -0,0 +1,59 @@
+class Solution {
+public:
+    string s;
+    int i = 0;
+
+    set<string> parseExpression() {
+        set<string> res = parseTerm();
+
+        while (i < s.size() && s[i] == ',') {
+            i++; // skip ','
+
+            set<string> next = parseTerm();
+            res.insert(next.begin(), next.end());
+        }
+
+        return res;
+    }
+
+    set<string> parseTerm() {
+        set<string> res = {""};
+
+        while (i < s.size() && s[i] != '}' && s[i] != ',') {
+            set<string> cur;
+
+            if (s[i] == '{') {
+                i++; // skip '{'
+
+                cur = parseExpression();
+
+                i++; // skip '}'
+            } else {
+                cur.insert(string(1, s[i]));
+                i++;
+            }
+
+            // Cartesian product: res × cur
+            set<string> next;
+
+            for (const string& a : res) {
+                for (const string& b : cur) {
+                    next.insert(a + b);
+                }
+            }
+
+            res = next;
+        }
+
+        return res;
+    }
+
+    vector<string> braceExpansionII(string expression) {
+        s = expression;
+        i = 0;
+
+        set<string> ans = parseExpression();
+
+        return vector<string>(ans.begin(), ans.end());
+    }
+};
